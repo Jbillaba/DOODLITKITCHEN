dc4e1b1c8798af773d97a60f5c2628b311f48e7f
Jeremy <jeremyvillalva66@gmail.com>
2026-09-10 19:01:13 -0400

    added: testing views has been added, simple test ensuring adoodle can be created with a owner

--- doodleit/cram_project/tests/test_views.py
+++ doodleit/cram_project/tests/test_views.py
@@ -1,8 +1,28 @@
 from django.test import TestCase
 from cram.models import Doodle
+from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+User = get_user_model()
 
 class DoodleViewTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
-        pass
+        cls.user = User.objects.create_user(
+            username='testingDoodlr',
+            email='testing@email.com',
+            password='hdOrderpicker1!'
+        )
+
+        cls.testDoodle = Doodle.objects.create(
+            title='test doodle',
+            image='testing.png',
+            doodlr=cls.user
+        )
+    
+    def test_doodle_has_doodlr(self):
+        print("Method: testing doodle has doodlr ")
+        doodle=Doodle.objects.get(id=self.testDoodle.id)
+
+        self.assertEqual(doodle.doodlr, self.user)
+        self.assertEqual(doodle.doodlr.username, 'testingDoodlr')
