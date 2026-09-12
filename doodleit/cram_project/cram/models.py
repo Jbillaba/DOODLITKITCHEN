7110693bec1cc96c269c2973ea31e1ac145e4a3f
Jeremy <jeremyvillalva66@gmail.com>
2026-09-11 20:38:17 -0400

    edited: User profile pictures should upload into the profile picture folder

--- doodleit/cram_project/cram/models.py
+++ doodleit/cram_project/cram/models.py
@@ -20,7 +20,8 @@ class UUIDTaggedItem(GenericUUIDTaggedItemBase, TaggedItemBase):
 
 class User(AbstractUser):
     id=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    profile_picture=models.FileField(default="profile_pictures/default_picture.png")
+    profile_picture=models.FileField(default="profile_pictures/default_picture.png",
+        upload_to='profile_pictures/')
     username=models.CharField(max_length=20, unique=True)
     email=models.EmailField(max_length=40, unique=True)
     password=models.CharField(max_length=128)
